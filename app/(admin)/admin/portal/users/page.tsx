8d8b64c181ef7e8c383e62df64bc8976359649f3
selvakumar-developer <selvatechy7@gmail.com>
2025-04-20 20:24:38 +0530

    feat: KAN-113 Created basic user list UI with API integration

--- app/(admin)/admin/portal/users/page.tsx
+++ app/(admin)/admin/portal/users/page.tsx
@@ -1,13 +1,19 @@
 import { DataTable } from "@/components/ui/data-table";
 import { GET_ALL_USERS } from "@/gql/admin";
 import { query } from "@/lib/client";
+import { ROUTE_PATH } from "@/lib/constant";
+import { redirect } from "next/navigation";
 import { columns } from "./components/columns";
 
 async function Page() {
+  try {
     const { data } = await query({ query: GET_ALL_USERS });
-    return (
-        <DataTable columns={columns} data={data.users} />
-    )
+    return <DataTable columns={columns} data={data.users} />;
+  } catch (error: any) {
+    if (error.message === "Unauthorized") {
+      redirect(ROUTE_PATH.ADMIN.login);
+    }
+  }
 }
 
-export default Page
+export default Page;
